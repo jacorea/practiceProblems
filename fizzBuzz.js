5338262e03176b8ed86412312d0bf3ad4daac9db
jcorea <jacorea410@gmail.com>
2019-04-17 10:33:08 -0700

    first commit

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,15 +3,15 @@
 //Date: 17 April, 2019
 //Completed by: Jonathan Corea
 
-// for(let i=0;i<=100; i++){
-//     if(i%3===0 & i%5===0){
-//         console.log("FizzBuzz")
-//     }else if(i%3 === 0){
-//         console.log("Fizz")
-//     } else if(i%5 ===0){
-//         console.log("Buzz")
-//     } else {
-//         console.log(i)
-//     }
-// }
+for(let i=0;i<=100; i++){
+    if(i%3===0 & i%5===0){
+        console.log("FizzBuzz")
+    }else if(i%3 === 0){
+        console.log("Fizz")
+    } else if(i%5 ===0){
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
 
