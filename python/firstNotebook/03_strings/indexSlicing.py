63d7943c9c78ae2650dd977e11f70745a2d5b13c
jcorea <jacorea410@gmail.com>
2019-07-17 10:58:44 -0700

    string slicing and indexing.

---
+++ python/firstNotebook/03_strings/indexSlicing.py
@@ -0,0 +1,24 @@
+myString = "Hello World"
+
+print(myString[0])
+
+myString = 'abcdefghijk'
+
+#practice - only want information from c onward.
+
+print((myString[2:]))
+
+# practice - only grabbing subsection of the string.
+print((myString[3:6]))
+
+# stepwise slicing 
+print((myString[::2]))
+
+# reversing string in python 
+print((myString[::-1]))
+
+#Practice
+    # return "r"from "Hello World" string 
+print('Hello World'[8])
+    # return 'ink' from 'tinker'
+print('tinker'[1:4])
---
+++ python/firstNotebook/03_strings/strings.py
@@ -0,0 +1,12 @@
+# strings 
+print('hello Jonathan')
+
+# new line
+    # "/n + word"
+
+
+#new tab 
+    # "/t + word"
+print("hello /tworld")
+
+#len- checks length of string.
