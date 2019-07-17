5fa4377daeb5db636caa2a05df917445d9036b32
jcorea <jacorea410@gmail.com>
2019-07-17 10:34:30 -0700

    variable assignments lesson.

---
+++ python/firstNotebook/02_variableAssignments/varAssignments.py
@@ -0,0 +1,15 @@
+# variables
+a = 5
+a = 10
+print(a+a)
+
+# checking variable type 
+
+print(type(a))
+
+# multiplying variables 
+my_income = 100 
+tax_rate = 0.1 
+my_taxes = my_income * tax_rate 
+
+print(my_taxes)
