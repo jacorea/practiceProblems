d3e39f2b7a041e1bf4d924e57f3051175f246606
jcorea <jacorea410@gmail.com>
2019-07-17 14:59:55 -0700

    added mathjs library to evaluation number expressions. Basic calculator finished.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,10 @@ import Button from './components/Button'
 import { Input } from './components/Input'
 import { ClearButton } from './components/ClearButton'
 
+
+
+import * as math from 'mathjs';
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -15,6 +19,10 @@ class App extends Component {
   
   addToInput = val => {
     this.setState({input: this.state.input + val})
+  }
+
+  handleEqual = () => {
+    this.setState({input: math.evaluate(this.state.input)})
   }
  
 render() {
@@ -44,7 +52,7 @@ render() {
         <div className="row">
           <Button handleClick={this.addToInput}>.</Button>
           <Button handleClick={this.addToInput}>0</Button>
-          <Button handleClick={this.addToInput}>=</Button>
+          <Button handleClick={()=> this.handleEqual()}>=</Button>
           <Button handleClick={this.addToInput}>-</Button>
         </div>
         <ClearButton handleClear={()=> this.setState({input: ""})}>Clear</ClearButton>
