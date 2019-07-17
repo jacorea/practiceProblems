4b59b1480e3e85cfcfeb80e0e12e6c1043def135
jcorea <jacorea410@gmail.com>
2019-07-17 11:25:39 -0700

    changed file name to order notebook. Also practiced string concatenation.

---
+++ python/firstNotebook/03_strings/stringMethods.py
@@ -0,0 +1,16 @@
+# concat
+# split 
+
+x = 'Hello World'
+
+print(x.upper())
+# split method
+print(x.split())
+
+# format method 
+print('the {2} {1} {0}'.format('fox','brown','quick'))
+result = 100/77 
+print('The result was {r:1.2f}'.format(r=result))
+print(f'My result is {result:1.2f}')
+
+print('I like %s' %'apples')
